8c286855eb5dedfa8828c806d96c00043be2c0d1
deepak-redsoftware <deepak.barwal@redsoftware.in>
2023-10-01 12:11:53 +0530

    Added error handler middleware and using prettier code formatting

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,16 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-const PORT =  process.env.PORT;
+const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const SALT_ROUNDS = process.env.SALT_ROUNDS;
 const NODE_ENV = process.env.NODE_ENV;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export {
-    PORT,
-    MONGO_URI,
-    SALT_ROUNDS,
-    NODE_ENV,
-    JWT_SECRET
-}
+export { PORT, MONGO_URI, SALT_ROUNDS, NODE_ENV, JWT_SECRET };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import helmet from 'helmet';
-import {PORT} from './config/server-config.js';
-import connectDB from './config/database.js';
+import express from "express";
+import helmet from "helmet";
+import { PORT } from "./config/server-config.js";
+import connectDB from "./config/database.js";
+import { notFound, errorHandler } from "./middlewares/error-middleware.js";
 
 const app = express();
 
@@ -10,11 +11,15 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Error Handler
+app.use(notFound);
+app.use(errorHandler);
+
 async function startServer() {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log('Server listening on port ' + PORT);
-    });
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+  });
 }
 
 startServer();
